a1d805ab76007e55f80771442d3a11cd2d62d405
Madhuyempuluru <61047887+madhu-cloud@users.noreply.github.com>
2025-06-03 13:34:07 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devesecops002" # Replace with your actual S3 bucket name
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    key    = "terraformdynamodb/terraform.tfstate"
+    region = "us-east-1"
   }
 }
